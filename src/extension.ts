e0af9b739f2ab8f2da58e3f3eb55f808dc478f89
Yubo-Cao <cao2006721@gmail.com>
2023-04-22 10:12:13 -0400

    feat: add debug

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { TextDecoder } from 'node:util';
 import * as vscode from 'vscode';
 
 
@@ -31,7 +30,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const inputFilePath = `${folderPath}\\1.in`;
 		const outputFilePath = `${folderPath}\\1.out`;
-		const sourceFilePath = `${folderPath}\\main.${language}`;
+		const sourceFilePath = `${folderPath}\\main.${{
+			java: 'java',
+			python: 'py',
+			cpp: 'cpp'
+		}[language]}`;
 		await Promise.all([
 			vscode.workspace.fs.writeFile(vscode.Uri.file(inputFilePath), new Uint8Array),
 			vscode.workspace.fs.writeFile(vscode.Uri.file(outputFilePath), new Uint8Array),
@@ -42,17 +45,16 @@ export function activate(context: vscode.ExtensionContext) {
 		const viewColumn = vscode.ViewColumn.One;
 		if (closeOtherWindowsWhenOpen) {
 			await vscode.commands.executeCommand('workbench.action.closeOtherEditors');
+			const inputDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(inputFilePath));
+			const outputDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(outputFilePath));
+			const sourceDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(sourceFilePath));
+
+			await Promise.all([
+				vscode.window.showTextDocument(sourceDocument, viewColumn),
+				vscode.window.showTextDocument(inputDocument, { viewColumn: viewColumn + 1, preserveFocus: true, preview: false }),
+				vscode.window.showTextDocument(outputDocument, { viewColumn: viewColumn + 1, preserveFocus: true, preview: false })
+			]);
 		}
-
-		const inputDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(inputFilePath));
-		const outputDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(outputFilePath));
-		const sourceDocument = await vscode.workspace.openTextDocument(vscode.Uri.file(sourceFilePath));
-await Promise.all([
-			vscode.window.showTextDocument(sourceDocument, viewColumn),
-			vscode.window.showTextDocument(inputDocument, { viewColumn: viewColumn + 1, preserveFocus: true, preview: false }),
-			vscode.window.showTextDocument(outputDocument, { viewColumn: viewColumn + 1, preserveFocus: true, preview: false })
-		]);
-
 
 		config.update('defaultCompetitionName', competitionName, true);
 		config.update('defaultLanguage', language, true);
@@ -60,6 +62,93 @@ await Promise.all([
 	});
 	context.subscriptions.push(disposable);
 
+	let debugDisposable = vscode.commands.registerCommand('codequest-bytebuster.launchDebug', async () => {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active text editor found');
+			return;
+		}
+
+		// Get the file path of the active text editor
+		const filePath = editor.document.uri.fsPath;
+
+		// Get the directory path of the active text editor
+		const dirPath = filePath.substring(0, filePath.lastIndexOf('\\'));
+
+		// Get the stem of the active text editor
+		const stem = filePath.substring(filePath.lastIndexOf('\\') + 1, filePath.lastIndexOf('.'));
+
+		// Check if 1.in and 1.out files exist in the same directory with the same stem
+		const inputFilePath = `${dirPath}\\1.in`;
+		const outputFilePath = `${dirPath}\\1.out`;
+		const inputExists = await vscode.workspace.fs.stat(vscode.Uri.file(inputFilePath)).then(() => true, () => false);
+		const outputExists = await vscode.workspace.fs.stat(vscode.Uri.file(outputFilePath)).then(() => true, () => false);
+		if (!inputExists || !outputExists) {
+			vscode.window.showErrorMessage('1.in or 1.out file not found');
+			return;
+		}
+
+		// Run the file with corresponding commands based on the language
+		const language = filePath.substring(filePath.lastIndexOf('.') + 1);
+		if (language === 'cpp') {
+			const executableFilePath = `${dirPath}\\${stem}.exe`;
+			const terminal = vscode.window.createTerminal({ name: 'CodeQuest ByteBuster Debug' });
+			terminal.show();
+			terminal.sendText(`g++ -std=c++17 -Wall -Wextra -pedantic-errors -o "${executableFilePath}" "${filePath}"`);
+			terminal.sendText(`Get-Content "${inputFilePath}" | "${executableFilePath}"`);
+			const output = await new Promise<string>(resolve => {
+				const interval = setInterval(async () => {
+					const outputBuffer = await vscode.workspace.fs.readFile(vscode.Uri.file(outputFilePath));
+					const output = new TextDecoder().decode(outputBuffer);
+					if (output.trim() !== '') {
+						clearInterval(interval);
+						resolve(output);
+					}
+				}, 100);
+			});
+			const outputDocument = await vscode.workspace.openTextDocument({ content: output });
+			await vscode.commands.executeCommand('vscode.diff', outputDocument.uri, vscode.Uri.file(outputFilePath), 'Output Diff');
+		} else if (language === 'py') {
+			const terminal = vscode.window.createTerminal({ name: 'CodeQuest ByteBuster Debug' });
+			terminal.show();
+			terminal.sendText(`Get-Content "${inputFilePath}" | python "${filePath}"`);
+			const output = await new Promise<string>(resolve => {
+				const interval = setInterval(async () => {
+					const outputBuffer = await vscode.workspace.fs.readFile(vscode.Uri.file(outputFilePath));
+					const output = new TextDecoder().decode(outputBuffer);
+					if (output.trim() !== '') {
+						clearInterval(interval);
+						resolve(output);
+					}
+				}, 100);
+			});
+			const outputDocument = await vscode.workspace.openTextDocument({ content: output });
+			await vscode.commands.executeCommand('vscode.diff', outputDocument.uri, vscode.Uri.file(outputFilePath), 'Output Diff');
+		} else if (language === 'java') {
+			const terminal = vscode.window.createTerminal({ name: 'CodeQuest ByteBuster Debug' });
+			terminal.show();
+			terminal.sendText(`javac "${filePath}"`);
+			const classFilePath = `${dirPath}\\${stem}.class`;
+			terminal.sendText(`Get-Content "${inputFilePath}" | java -classpath "${dirPath}" "${stem}" | Out-File -FilePath "${classFilePath}"`);
+			const output = await new Promise<string>(resolve => {
+				const interval = setInterval(async () => {
+					const outputBuffer = await vscode.workspace.fs.readFile(vscode.Uri.file(classFilePath));
+					const output = new TextDecoder().decode(outputBuffer);
+					if (output.trim() !== '') {
+						clearInterval(interval);
+						resolve(output);
+					}
+				}, 100);
+			});
+			const outputDocument = await vscode.workspace.openTextDocument({ content: output });
+			await vscode.commands.executeCommand('vscode.diff', outputDocument.uri, vscode.Uri.file(outputFilePath), 'Output Diff');
+		} else {
+			vscode.window.showErrorMessage('Unsupported language');
+			return;
+		}
+	});
+	context.subscriptions.push(debugDisposable);
 }
 
 // This method is called when your extension is deactivated
